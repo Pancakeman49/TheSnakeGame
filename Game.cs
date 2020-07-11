01e7b7d4228ee02f9361cce0266d28b22acf8f69
TheRealPancakeman <rolands.silins345@gmail.com>
2020-07-11 16:13:58 +0300

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheSnakeGame
+{
+    public partial class Game : Form
+    {
+        Area area = new Area();
+        Snake snake = new Snake();
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            this.Height = 600;
+            this.Width = 600;
+            
+            this.Controls.Add(area);
+            area.Top = 100;
+            area.Left = 100;
+            //area.Location = new Point(100, 100);
+
+            //adding snake body
+        }
+
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheSnakeGame
+{
+    class Snake
+    {
+        public List<PictureBox> snakePixels = new List<PictureBox>();
+
+        public Snake()
+        {
+            InitializeSnake();
+        }
+
+        private void InitializeSnake()
+        {
+            PictureBox snakePixel = new PictureBox();
+            snakePixel.Height = 20;
+            snakePixel.Width = 20;
+            snakePixel.BackColor = Color.Orange;
+            snakePixels.Add(snakePixel);
+        }
+    }
+}
